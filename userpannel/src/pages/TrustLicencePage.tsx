4996dd337a88e1152f6f56d00838e83519448739
saicharan2442 <ffmails500@email.com>
2025-05-08 11:52:52 +0530

    first commit

--- userpannel/src/pages/TrustLicencePage.tsx
+++ userpannel/src/pages/TrustLicencePage.tsx
@@ -23,25 +23,7 @@ const TrustLicencePage = () => {
               className="w-full max-w-[210mm] h-auto md:max-w-[80%] lg:max-w-[70%] xl:max-w-[60%] shadow-lg rounded-md"
             />
           </div>
-          <div className="bg-white rounded-lg shadow-md p-8 border border-gold-light/30 flex justify-center items-center">
-            
-            {/* A4 Size Image with Mobile Responsiveness */}
-            <img
-              src="/src/pages/images/doc1.jpg" 
-              alt="Trust Licence Document"
-              className="w-full max-w-[210mm] h-auto md:max-w-[80%] lg:max-w-[70%] xl:max-w-[60%] shadow-lg rounded-md"
-            />
-          </div>
-          <div className="bg-white rounded-lg shadow-md p-8 border border-gold-light/30 flex justify-center items-center">
-            
-            {/* A4 Size Image with Mobile Responsiveness */}
-            <img
-              src="/src/pages/images/doc2.jpg" 
-              alt="Trust Licence Document"
-              className="w-full max-w-[210mm] h-auto md:max-w-[80%] lg:max-w-[70%] xl:max-w-[60%] shadow-lg rounded-md"
-            />
-          </div>
-        
+          
         </div>
       </div>
       <Footer />
